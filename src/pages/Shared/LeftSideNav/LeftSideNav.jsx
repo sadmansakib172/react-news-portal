a3912ff13054e428797a9f54aa06c9dd1bf40a08
sadmansakib172 <salim15-9754@diu.edu.bd>
2023-10-03 12:58:39 +0600

    left side bar added

--- src/pages/Shared/LeftSideNav/LeftSideNav.jsx
+++ src/pages/Shared/LeftSideNav/LeftSideNav.jsx
@@ -1,9 +1,26 @@
+import { useEffect, useState } from "react";
+import { NavLink } from "react-router-dom";
 
 
 const LeftSideNav = () => {
+    const [categories, setCategories]=useState([])
+    useEffect(()=>{
+        fetch('categories.json')
+        .then(res=>res.json())
+        .then(data=>setCategories(data))
+    },[])
     return (
         <div>
-            <h2 className="text-2xl">Left side</h2>
+            <h2 className="text-2xl">All Category</h2>
+            <div className="space-y-8 mt-6">
+                {
+                    categories.map(category => <NavLink
+                         className="block ml-4 text-xl font-medium"
+                          key={category.id}
+                          to={`category/${category.id}`}
+                          >{category.name}</NavLink>)
+                }
+            </div>
         </div>
     );
 };
