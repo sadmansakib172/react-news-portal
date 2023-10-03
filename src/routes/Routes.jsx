07a2895a584eb60e23f0f44da01566e6065199c7
sadmansakib172 <salim15-9754@diu.edu.bd>
2023-10-03 18:47:44 +0600

    new login route created

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Root from "../layouts/Root";
 import Home from "../pages/Home/Home";
+import Login from "../pages/Login/Login";
 
 
 
@@ -12,6 +13,10 @@ const router = createBrowserRouter([
             {
                 path:'/',
                 element:<Home></Home>
+            },
+            {
+                path:'/login',
+                element:<Login></Login>
             }
         ]
        
